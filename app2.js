ad69d3930c977088fdcc2f791f95fc581d62b7b6
hudsonrnb <hudsonaaac@outlook.com>
2025-10-02 18:40:25 +0800

    Update and rename app明文.js to app2.js

--- app2.js
+++ app2.js
@@ -8,7 +8,7 @@ const { Buffer } = require('buffer');
 const { exec, execSync } = require('child_process');
 
 // 环境变量
-const UUID = process.env.UUID || 'a2056d0d-c98e-4aeb-9aab-37f64edd5710'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
+const UUID = process.env.UUID || 'edd49ac9-b0b3-4b24-81af-a441c3787063'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
@@ -17,7 +17,7 @@ const XPATH = process.env.XPATH || UUID.slice(0, 8);       // xhttp路径,自动
 const SUB_PATH = process.env.SUB_PATH || `${UUID}`;        // 节点订阅路径,默认位uuid
 const DOMAIN = process.env.DOMAIN || '';                   // 域名或ip,留空将自动获取服务器ip
 const NAME = process.env.NAME || 'Hug';                    // 节点名称
-const PORT = process.env.PORT || 7860;                     // http服务                   
+const PORT = process.env.PORT || 7981;                     // http服务                   
 
 // 核心配置
 const SETTINGS = {
